b5de8edb542b5ad23f219cd343beacdef1b7b254
devipriya <devipriyak1955@gmail.com>
2025-12-18 11:49:47 +0530

    deployed

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ connectDB();
 app.use(cors({
   origin: [
     process.env.FRONTEND_URL,
-    'http://localhost:5173'
+    'http://localhost:5173',"https://netflix-project-hazel.vercel.app"
   ],
   credentials: true
 }));
